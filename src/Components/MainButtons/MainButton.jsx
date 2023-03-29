32bc37665fe07e727aa543c8066fbbb74d9939a6
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-03-28 23:11:51 -0600

    Actualizar componente

--- src/Components/MainButtons/MainButton.jsx
+++ src/Components/MainButtons/MainButton.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
 
-import './MainButtonStyle.scss'
+import "./MainButtonStyle.scss";
 
-
-export default function MainButton(props){
-    return (
-       <div className="containerButton"> 
-         <h1>{props.icon}</h1>
-         <p>{props.text}</p>
-       </div>
-    );
+export default function MainButton(props) {
+  return (
+    <div className="containerButton">
+      <Link to={props.link}>
+        <h1>{props.icon}</h1>
+        <p>{props.text}</p>
+      </Link>
+    </div>
+  );
 }
