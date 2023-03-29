88525cb5bf37e1cff92437f5542e8f05d501bea3
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-03-28 23:28:38 -0600

    Actualizar mapeo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,26 @@ import GetUserInfo from "./Components/GetUserInfo/GetUserInfo";
 import Diet from "./Pages/Diet/Diet";
 
 function App() {
+  const views = [
+    {
+      name: "Home",
+      path: "/",
+      component: GetUserInfo,
+    },
+    {
+      name: "Diet",
+      path: "/diet",
+      component: Diet,
+    },
+  ];
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<GetUserInfo />} />
-        <Route path="/diet" element={<Diet />} />
+        {views.map((view) => (
+          <Route key={view.name} path={view.path} element={view.component()} />
+        ))}
       </Routes>
       <Footer />
     </>
