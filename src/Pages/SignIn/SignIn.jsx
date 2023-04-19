15ff45033a1f5539244cbeea49dc7b0e40bfcd12
Javier Lopez <javierlopezguzman00@gmail.com>
2023-04-19 08:21:25 -0600

    cambios

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import './SignIn.scss'
+import axios from "axios";
 
 export default function SignIn(){
+    useEffect(() => {
+        
+        axios.request(options)
+          .then(response => setExercises(response.data))
+          .catch(error => console.error(error));
+      }, [muscleSelected]);
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -18,7 +25,36 @@ export default function SignIn(){
         event.preventDefault();
         console.log("Username:", username);
         console.log("Password:", password);
-    }
+      
+        const options = {
+          method: 'POST',
+          url: 'https://example.com/login', // Cambiar esto con la URL del servidor de autenticación
+          data: {
+            username: username,
+            password: password
+          },
+          headers: {
+            'Content-Type': 'application/json',
+            //'Authorization': 'Bearer ' + token
+          }
+        };
+      
+        axios(options)
+          .then((response) => {
+            if (response.status === 200) {
+              // Las credenciales del usuario son correctas, permitir acceso
+              console.log('Inicio de sesión exitoso');
+              // Agregar aquí la lógica para redirigir al usuario a la funcionalidad deseada
+            } else {
+              // Las credenciales del usuario son incorrectas
+              console.log('Credenciales incorrectas');
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+      
 
     return(
         <div className="mainSigInBox">
