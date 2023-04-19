9f0dccad058a5a39b53f3b37cdda202df8cbddfc
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-04-19 12:12:08 -0600

    Actualizar navigate

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -31,7 +31,7 @@ export default function SignIn(){
 
               setUsername("");
               setPassword("");
-              history("/Register");
+              history("/diet");
             });
 
     }
